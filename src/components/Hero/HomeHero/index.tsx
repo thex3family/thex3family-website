f07b0156ca00261bd93de0921345ea9d444a6b87
Conrad Lin <git@conradlin.com>
2024-01-27 22:43:28 +0800

    Fix link typo

--- src/components/Hero/HomeHero/index.tsx
+++ src/components/Hero/HomeHero/index.tsx
@@ -38,7 +38,7 @@ const HomeHero = ({ heroImg }: HomeHeroProps) => {
               {t("page-index:page-index-title")}
             </Heading>
             <Text size="xl">{t("page-index:page-index-description")}</Text>
-            <ButtonLink href="/live-your-best-life/understand-yourself">
+            <ButtonLink href="/live-your-best-life/about-us">
               {t("common:get-started")}
             </ButtonLink>
           </VStack>
