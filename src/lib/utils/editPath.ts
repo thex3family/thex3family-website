dfad197787a5d7bf16b94175a9c69d698e097020
Conrad Lin <git@conradlin.com>
2024-01-28 00:27:31 +0800

    Fix github link

--- src/lib/utils/editPath.ts
+++ src/lib/utils/editPath.ts
@@ -2,4 +2,4 @@ import { join } from 'path'
 
 import { CONTENT_DIR, EDIT_CONTENT_URL } from '@/lib/constants'
 
-export const getEditPath = (relativePath: string): string => join(EDIT_CONTENT_URL, CONTENT_DIR, relativePath, "index.md")
+export const getEditPath = (relativePath: string): string => `${EDIT_CONTENT_URL}/${CONTENT_DIR}/${relativePath}/index.md`;
