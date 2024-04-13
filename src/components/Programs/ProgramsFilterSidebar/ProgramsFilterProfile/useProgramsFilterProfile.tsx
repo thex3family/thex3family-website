d7d6eb5490b9d5e21bae50286ade82ce660af471
Conrad Lin <admin@conradlin.com>
2024-04-13 21:00:25 +0800

    Add additional filtering options

--- src/components/Programs/ProgramsFilterSidebar/ProgramsFilterProfile/useProgramsFilterProfile.tsx
+++ src/components/Programs/ProgramsFilterSidebar/ProgramsFilterProfile/useProgramsFilterProfile.tsx
@@ -42,57 +42,57 @@ export const useFrameworkFilterProfile = (setSelectedTags) => {
         // filterLabels.one,
       ],
       presetFilters: {
-        LEVEL_1: false,
-        LEVEL_2: false,
+        LEVEL_1: true,
+        LEVEL_2: true,
         LEVEL_3: true,
         LEVEL_4: true,
+        LEVEL_5: true,
+        LEVEL_6: true,
+        LEVEL_7: true,
+        LEVEL_8: true,
+        LEVEL_9: true,
+        tags: ["motivation"]
+      },
+    },
+    {
+      title: t("page-programs:page-programs-profile-2-title"),
+      description: t("page-programs:page-programs-profile-2-description"),
+      featureHighlight: [
+        // filterLabels.one,
+      ],
+      presetFilters: {
+        LEVEL_1: false,
+        LEVEL_2: false,
+        LEVEL_3: false,
+        LEVEL_4: false,
         LEVEL_5: false,
         LEVEL_6: false,
         LEVEL_7: false,
         LEVEL_8: false,
         LEVEL_9: false,
-        tags: ["gamification"]
+        tags: ["relationships"]
       },
     },
-    // {
-    //   title: t("page-programs:page-programs-profile-2-title"),
-    //   description: t("page-programs:page-programs-profile-2-description"),
-    //   featureHighlight: [
-    //     // filterLabels.one,
-    //   ],
-    //   presetFilters: {
-    //     LEVEL_1: false,
-    //     LEVEL_2: false,
-    //     LEVEL_3: false,
-    //     LEVEL_4: false,
-    //     LEVEL_5: false,
-    //     LEVEL_6: false,
-    //     LEVEL_7: false,
-    //     LEVEL_8: false,
-    //     LEVEL_9: false,
-    //     tags: ["relationships"]
-    //   },
-    // },
-    // {
-    //   title: t("page-programs:page-programs-profile-3-title"),
-    //   description: t("page-programs:page-programs-profile-3-description"),
-    //   featureHighlight: 
-    //     [
-    //       // filterLabels.one,
-    //     ],
-    //   presetFilters: {
-    //     LEVEL_1: false,
-    //     LEVEL_2: false,
-    //     LEVEL_3: false,
-    //     LEVEL_4: false,
-    //     LEVEL_5: false,
-    //     LEVEL_6: false,
-    //     LEVEL_7: false,
-    //     LEVEL_8: false,
-    //     LEVEL_9: false,
-    //     tags: []
-    //   },
-    // },
+    {
+      title: t("page-programs:page-programs-profile-3-title"),
+      description: t("page-programs:page-programs-profile-3-description"),
+      featureHighlight: 
+        [
+          // filterLabels.one,
+        ],
+      presetFilters: {
+        LEVEL_1: false,
+        LEVEL_2: false,
+        LEVEL_3: false,
+        LEVEL_4: false,
+        LEVEL_5: false,
+        LEVEL_6: false,
+        LEVEL_7: false,
+        LEVEL_8: false,
+        LEVEL_9: false,
+        tags: ["self-love"]
+      },
+    },
     {
       title: t("page-programs:page-programs-profile-4-title"),
       description: t("page-programs:page-programs-profile-4-description"),
